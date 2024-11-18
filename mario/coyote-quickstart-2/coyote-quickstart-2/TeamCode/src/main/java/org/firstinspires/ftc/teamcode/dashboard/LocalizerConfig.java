5452bde8c3a1841b4f8f507dbe09c1579ef66886
davidcarciumaru21 <129268173+davidcarciumaru21@users.noreply.github.com>
2024-11-18 17:06:23 +0200

    Add files via upload

---
+++ mario/coyote-quickstart-2/coyote-quickstart-2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dashboard/LocalizerConfig.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.dashboard;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class LocalizerConfig {
+    public static int TPR = 2000;
+
+    public static double WhlR = 0.024;
+    //
+    public static double LY =  0.09567839999999998;//0.108; //0.21187020000000503 0.108
+    public static double RY = -0.11429339999999977;//-0.108; //-0.22091160000000307
+    public static double CX = -0.16101119999999996;//-0.16; //-7.116000000000076E-4 0.16
+
+    public static int TuningTurns = 20;
+}
